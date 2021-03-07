b95bc2607df8fa66cd57c7f301b0eef8460f38c1
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-03-07 20:12:30 +0200

    finish splash screen

--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -1,13 +1,18 @@
 import React, { memo } from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Image } from 'react-native';
+import { useSplash } from 'src/hooks';
 import style from './style';
 
 const Splash = () => {
-    const { container } = style;
-    return (
-        <View style={container}>
+    const { container, subContainer, logo } = style();
+    useSplash();
 
-        </View>
+    return (
+        <ImageBackground style={container} source={require('src/assets/background.jpeg')} blurRadius={10}>
+            <View style={subContainer}>
+                <Image source={require('src/assets/logo.png')} style={logo} />
+            </View>
+        </ImageBackground>
     );
 }
 
--- src/screens/Splash/style.js
+++ src/screens/Splash/style.js
@@ -1,7 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, useWindowDimensions } from 'react-native';
 
-export default StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-});
+export default () => {
+    const { width } = useWindowDimensions();
+
+    const logo = Image.resolveAssetSource(require('src/assets/logo.png'));
+    const logoWidth = width * 80 / 100;
+    const logoHeight = (logo.height * logoWidth) / logo.width;
+
+    return StyleSheet.create({
+        container: {
+            flex: 1
+        },
+        subContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#000000bb'
+        },
+        logo: {
+            width: logoWidth,
+            height: logoHeight
+        }
+    });
+}
