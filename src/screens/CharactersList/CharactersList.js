755a47260bf80dfd08e23b2c3f8ec97d3c100530
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-03-07 21:28:19 +0200

    finish Characters list screen

--- src/screens/CharactersList/CharactersList.js
+++ src/screens/CharactersList/CharactersList.js
@@ -1,11 +1,55 @@
 import React, { memo } from 'react';
-import { View } from 'react-native';
+import { View, Image, TouchableOpacity, FlatList, Text, ActivityIndicator, RefreshControl, Modal } from 'react-native';
+import { BlurView } from "@react-native-community/blur";
+import { useCharactersList } from 'src/hooks';
 import style from './style';
 
 const CharactersList = () => {
-    const { container } = style();
+    const { container, toolbar, logo, searchIcon, searchButton, contentContainerStyle, indicator, heroCard, cover, footerView, cardTitle } = style();
+    const { loading, list, loadMore, isRefreshing, onRefresh, onEnd } = useCharactersList();
+
+    const renderItem = ({ item }) => {
+        return (
+            <TouchableOpacity activeOpacity={.8} style={heroCard}>
+                <Image source={{ uri: `${item?.thumbnail?.path}/landscape_xlarge.${item?.thumbnail?.extension}` || "https://insomniac.games/wp-content/uploads/2018/09/Spider-Man_PS4_Selfie_Photo_Mode_LEGAL.jpg" }} style={cover} />
+                <BlurView
+                    style={footerView}
+                    blurType="dark"
+                    blurAmount={1}
+                    reducedTransparencyFallbackColor="white"
+                >
+                    <Text style={cardTitle}>{item.name || "Spider Man"}</Text>
+                </BlurView>
+            </TouchableOpacity>
+        );
+    }
+
+    const renderFooter = () => {
+        return (
+            <ActivityIndicator color="red" style={{ marginVertical: '4%' }} />
+        );
+    }
+
     return (
         <View style={container}>
+            <View style={toolbar}>
+                <Image source={require('src/assets/logo.png')} style={logo} />
+                <TouchableOpacity activeOpacity={.8} style={searchButton}>
+                    <Image source={require('src/assets/magnify.png')} style={searchIcon} />
+                </TouchableOpacity>
+            </View>
+
+            {loading ? <ActivityIndicator color="red" style={indicator} /> : <FlatList
+                data={list}
+                renderItem={renderItem}
+                keyExtractor={(e, i) => i.toString()}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={contentContainerStyle}
+                onEndReachedThreshold={.2}
+                ListFooterComponent={loadMore && renderFooter}
+                onEndReached={onEnd}
+                refreshControl={<RefreshControl colors={["red", "#fff"]} tintColor="red" refreshing={isRefreshing} onRefresh={onRefresh} />}
+            />}
         </View>
     );
 }
--- src/screens/CharactersList/style.js
+++ src/screens/CharactersList/style.js
@@ -1,11 +1,80 @@
-import { StyleSheet, useWindowDimensions } from 'react-native';
+import { StyleSheet, useWindowDimensions, Image, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default () => {
     const { width } = useWindowDimensions();
+    const { top, bottom } = useSafeAreaInsets();
+
+    const logo = Image.resolveAssetSource(require('src/assets/logo.png'));
+    const logoWidth = width * 40 / 100;
+    const logoHeight = (logo.height * logoWidth) / logo.width;
 
     return StyleSheet.create({
         container: {
-            flex: 1
+            flex: 1,
+            backgroundColor: "#000"
+        },
+        toolbar: {
+            width: '100%',
+            height: top + logoHeight + 10,
+            backgroundColor: '#111',
+            borderBottomLeftRadius: 40,
+            borderBottomRightRadius: 40,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: '6%'
+        },
+        logo: {
+            width: logoWidth,
+            height: logoHeight
+        },
+        searchButton: {
+            width: 44,
+            height: 44,
+            backgroundColor: 'transparent',
+            position: 'absolute',
+            top: '60%',
+            right: '2%',
+            borderRadius: 22
+        },
+        searchIcon: {
+            tintColor: "red",
+            width: 20,
+            height: 20
+        },
+        contentContainerStyle: {
+            paddingTop: 10,
+            paddingBottom: bottom + 10
+        },
+        heroCard: {
+            width: '90%',
+            height: 200,
+            backgroundColor: 'transparent',
+            alignSelf: 'center',
+            marginVertical: '2%',
+            borderRadius: 20,
+            overflow: 'hidden'
+        },
+        cover: {
+            width: '100%',
+            height: '100%'
+        },
+        footerView: {
+            width: '100%',
+            height: 40,
+            position: 'absolute',
+            bottom: 0,
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        cardTitle: {
+            fontSize: 16,
+            color: '#fff',
+            fontWeight: "bold"
+        },
+        indicator: {
+            marginTop: '30%'
         }
     });
 }
