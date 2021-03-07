c42d0869c2ce97d8fb1a8541fe30d9c597e78378
Abdalla Emad <abdalla.emad.eldin@gmail.com>
2021-03-07 22:05:13 +0200

    add search api model

--- src/models/charactersList.model.js
+++ src/models/charactersList.model.js
@@ -4,3 +4,7 @@ import { PUBLIC_KEY } from '@env';
 export const getList = (limit, offset, options) => {
     get(`characters?apikey=${PUBLIC_KEY}&hash=${hash}&ts=1&offset=${offset}&limit=${limit}`, options);
 }
+
+export const searchByName = (keyward, options) => {
+    get(`characters?apikey=${PUBLIC_KEY}&hash=${hash}&ts=1&nameStartsWith=${keyward}`, options);
+}
